cd8d8756b32d45f23ce85b9aaa3a3e8aa0939ca7
Shaurya Awasthi <shaurya.awasthi130@gmail.com>
2025-07-05 23:57:06 +0530

    Still working on api

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -2,7 +2,7 @@ import { answerCollection, db, questionCollection, voteCollection } from "@/mode
 import { databases, users } from "@/models/server/config";
 import { UserPrefs } from "@/store/Auth";
 import { NextRequest, NextResponse } from "next/server";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 
 export async function POST(request: NextRequest) {
     try {
@@ -39,7 +39,40 @@ export async function POST(request: NextRequest) {
 
         //that means previous vote status does not exist or status hasbeen changed
         if(response.documents[0]?.voteStatus !== voteStatus){
+            const doc = await databases.createDocument(db, voteCollection, ID.unique(), {
+                type,
+                typeId,
+                voteStatus,
+                votedById,
+            });
 
+            // Increate/Decrease the reputation of the question/answer author accordingly
+            const questionOrAnswer = await databases.getDocument(
+                db,
+                type === "question" ? questionCollection : answerCollection,
+                typeId
+            );
+
+            const authorPrefs = await users.getPrefs<UserPrefs>(questionOrAnswer.authorId);
+
+            // if vote was present
+            if (response.documents[0]) {
+                await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+                    reputation:
+                        // that means prev vote was "upvoted" and new value is "downvoted" so we have to decrease the reputation
+                        response.documents[0].voteStatus === "upvoted"
+                            ? Number(authorPrefs.reputation) - 1
+                            : Number(authorPrefs.reputation) + 1,
+                });
+            } else {
+                await users.updatePrefs<UserPrefs>(questionOrAnswer.authorId, {
+                    reputation:
+                        // that means prev vote was "upvoted" and new value is "downvoted" so we have to decrease the reputation
+                        voteStatus === "upvoted"
+                            ? Number(authorPrefs.reputation) + 1
+                            : Number(authorPrefs.reputation) - 1,
+                });
+            }
         }
 
         const [upvotes, downvotes]= await Promise.all([
